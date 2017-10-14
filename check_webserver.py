c80ca342f12ba69b45dcfb2dfc752d4b7948dfc3
dimitraz <dimitrazuccarelli@gmail.com>
2017-10-14 13:37:56 +0100

    Adding try/catch blocks

--- check_webserver.py
+++ check_webserver.py
@@ -6,14 +6,22 @@ import subprocess
 
 def check_webserver():
     cmd = 'ps -A | grep nginx | grep -v grep'
-    status = subprocess.run(cmd, shell=True).returncode 
 
-    if status != 0: 
-        print ('Nginx not running, starting now..')
-        cmd = 'sudo service nginx start'
-        subprocess.run(cmd, shell=True) 
-    else:
-        print ('Nginx is running') 
+    try:
+        status = subprocess.run(cmd, shell=True).returncode 
+
+        if status != 0: 
+            print ('Nginx not running, starting now..')
+            cmd = 'sudo service nginx start'
+            subprocess.run(cmd, shell=True) 
+        else:
+            print ('Nginx is running') 
+    except Exception as e:
+        error = str(e)
+        print ('Error occurred:', error)
+
+def main():
+    check_webserver()
 
 if __name__ == '__main__':
-  check_webserver()
+  main()
