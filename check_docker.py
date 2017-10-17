0aec51413695abc7347f2a75b7c1f3d8d530b6f4
dimitraz <dimitrazuccarelli@gmail.com>
2017-10-17 15:13:49 +0100

    Update Docker script

--- check_docker.py
+++ check_docker.py
@@ -3,9 +3,10 @@
 # To do: Implement usability 
 
 import subprocess
+from helpers import ssh, scp
 
 def check_docker():
-    cmd = 'ps -A | grep docker | grep -v grep'
+    cmd = 'cd /home/ec2-user/ && ps -A | grep docker | grep -v grep'
 
     try:
         status = subprocess.run(cmd, shell=True).returncode 
@@ -20,8 +21,37 @@ def check_docker():
         error = str(e)
         print ('Error occurred:', error)
 
+def build_image():
+    tag = 'captain-hook'
+    cmd = 'cd /home/ec2-user/flask-app && sudo docker build -t ' + tag + ' .'
+    ssh('key.pem', '34.241.48.147', cmd)
+
+def start_container():
+    tag = 'captain-hook'
+    port = '80'
+    cmd = 'sudo docker run -d -p ' + port + ':80 ' + tag
+    ssh('key.pem', '34.241.48.147', cmd)
+
+def check_container():
+    tag = 'captain-hook'
+    cmd = 'sudo docker ps -a | grep ' + tag
+
+    try:
+        status = subprocess.run(cmd, shell=True).returncode 
+
+        if status != 0: 
+            print ('Container not running, starting now..')
+            build_image()
+            start_container()
+        else:
+            print ('Container is running') 
+    except Exception as e:
+        error = str(e)
+        print ('Error occurred:', error)
+
 def main():
     check_docker()
+    check_container()
 
 if __name__ == '__main__':
   main()
