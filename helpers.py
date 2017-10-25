ab8a2cde6fd80f4f7ca9e08951fa37492f292ce0
dimitraz <dimitrazuccarelli@gmail.com>
2017-10-25 18:05:24 +0100

    Updating ssh/scp helpers

--- helpers.py
+++ helpers.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
 
 import subprocess 
+import sys
 
 # ssh into instance and perform command
 def ssh(key, dns, cmd):
-    ssh = 'ssh -i ' + key + ' ec2-user@' + dns + 'rm -rf check_webserver.py' + cmd
-    subprocess.run(cmd, shell=True)
+    ssh = 'ssh -o StrictHostKeyChecking=no -i ' + key + '.pem ec2-user@' + dns + ' \'' + cmd + '\''
+    try: 
+        subprocess.run(ssh, shell=True)
+    except Exception as e:
+        print ('Error while connecting to host:', str(e))
+        sys.exit(1)
 
 # copy files and folders to a given destination
 def scp(key, dns, file, dest):
-    cmd = 'scp -r -i ' + key + ' ' + file + ' ec2-user@' + dns + ':' + dest
-    subprocess.run(cmd, shell=True)
+    cmd = 'scp -o StrictHostKeyChecking=no -r -i ' + key + '.pem ' + file + ' ec2-user@' + dns + ':' + dest
+    try: 
+        subprocess.run(cmd, shell=True)
+    except Exception as e:
+        print ('Error while connecting to host:', str(e))
+        sys.exit(1)
 
